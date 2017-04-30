e41fb373f9c084745e5b0c6c766664c02a560c54
Renault Young <nbamaniacforever@gmail.com>
2017-04-29 21:45:10 -0400

    More cors fixes

--- main.go
+++ main.go
@@ -100,7 +100,9 @@ func createSighting(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 }
 
 func optionsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+	}
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers",
 		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
